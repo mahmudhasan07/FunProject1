d6378c159fb9ed2b97c8fe656e6db05dc0ce5f26
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-30 05:18:59 +0600

    working on absolute position

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,76 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setmessage] = useState("Tumi ki amke valobasho ??");
+  const [postion, setpostion] = useState("relative");
+  const [unique, setunique] = useState("");
+  // const [top, settop] = useState(100);
+  // const [left, setleft] = useState(500);
+
+  let top = 100
+  let left = 500
+
+  const handleYES = () => {
+    setmessage("hehe, jantam tmi amke valobasho...")
+
+  }
+
+  const handleNO = () => {
+    if (message == 'Tumi ki amke valobasho ??') {
+      setmessage("vebe bolteso to...")
+    }
+    if (message == "vebe bolteso to...") {
+      setmessage("R ekbar valo moto vebe nao..")
+    }
+    if (message == "R ekbar valo moto vebe nao..") {
+
+      setunique("absolute")
+      console.log(typeof (top, left));
+      // settop(randomTop)
+      // setleft(randomLeft)
+      if (top == 100 && left == 500) {
+        // settop(randomTop)
+        settop(200)
+        setleft(700)
+        // setleft(randomLeft)
+        console.log("nai");
+        return
+      }
+      else {
+        settop(Math.random() * 1000)
+        setleft(Math.random() * 1000)
+        console.log("change hocche");
+      }
+    }
+
+    if (message == "bujse mair khaba") {
+      // setmessage("")
+    }
+
+  }
+
+  const handletopleft = () => {
+    const randomTop = parseInt(Math.random() * 1000)
+    const randomLeft = parseInt(Math.random() * 100)
+    // top = 300
+    // left = 900
+    setunique("absolute")
+  }
+
+
+  console.log(top, left);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className='h-screen items-center flex '>
+      <div className='space-y-10 border-2 w-1/3 mx-auto p-5 rounded-2xl text-center'>
+        <h1 className='text-3xl'>{message}</h1>
+        <div className='flex justify-center  gap-5'>
+          <button onClick={handleYES} className='btn'>Yes</button>
+          <button onClick={handleNO} className={`btn ${unique} top-[${top}px] left-[${left}px]`}>No</button>
+          <button className='btn' onClick={handletopleft}>handle</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </section>
   )
 }
 
