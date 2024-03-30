eb4b4a9782fe8832c204722c03ed3bede248bfab
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-30 07:07:41 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import qus from "../public/lovequs.gif";
+import accpect from "../public/accpect_love.gif";
+import final from "../public/simple crying.gif";
+import firstcry from "../public/emotinal.gif";
 
 function App() {
   const [count, setCount] = useState(0)
   const [message, setmessage] = useState("Tumi ki amke valobasho ??");
-  const [position, setposition] = useState("");
-  const [hover, sethover] = useState("handlehover");
-  const [top, settop] = useState(700);
-  const [left, setleft] = useState(500);
+  const [image, setimage] = useState(qus);
 
   // let top = 100
   // let left = 500
@@ -17,15 +18,18 @@ function App() {
   const handleYES = () => {
     setmessage("hehe, jantam tmi amke valobasho...")
     document.getElementById("NO").style.display = "none"
+    setimage(accpect)
 
   }
 
   const handleNO = () => {
     if (message == 'Tumi ki amke valobasho ??') {
       setmessage("vebe bolteso to...")
+      setimage(firstcry)
     }
     if (message == "vebe bolteso to...") {
       setmessage("R ekbar valo moto vebe nao..")
+      setimage(final)
     }
     if (message == "R ekbar valo moto vebe nao..") {
       const randomTop = parseInt(Math.random() * 500)
@@ -56,11 +60,12 @@ function App() {
 
   return (
     <section className='h-screen relative items-center flex border-2 border-white'>
-      <div className='space-y-10 border-2 w-1/3 mx-auto p-5 rounded-2xl text-center'>
+      <div className='space-y-5  border-2 w-1/3 mx-auto p-5 rounded-2xl text-center'>
+        <img src={image} className='border-2 mx-auto rounded-xl w-96' alt="" />
         <h1 className='text-3xl'>{message}</h1>
         <div className='flex justify-center  gap-5'>
-          <button onClick={handleYES} className='btn'>Yes</button>
-          <button id='NO' onClick={handleNO} className={`btn`}>No</button>
+          <button onClick={handleYES} className='btn btn-lg btn-primary'>Yes</button>
+          <button id='NO' onClick={handleNO} className={`btn btn-lg btn-primary`}>No</button>
           {/* <button className='btn' onClick={handletopleft}>handle</button> */}
         </div>
       </div>
